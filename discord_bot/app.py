360a384693ddfa775b05b75e260703bfe5e7e272
Sung96kim <sung96kim@gmail.com>
2022-08-02 16:46:38 -0400

    init

---
+++ discord_bot/app.py
@@ -0,0 +1,17 @@
+import discord
+from dotenv import load_dotenv
+import os
+load_dotenv() 
+
+pub_key = os.getenv("PUBLIC_KEY")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+
+client.run(pub_key)
